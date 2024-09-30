2656395f05a7b947936e52c615048a01d1756fe9
Geet George <g.george@tudelft.nl>
2024-09-30 16:56:54 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Sum2
+
+This is a project that has functions to add two numbers.
+
+## Example / Tutorial
+
+You can call the function `add_two_numbers1` as shown:
+
+`add_two_numbers(5,10)`
+
+and it will print
+
+`15`.
---
+++ myfunctions.py
@@ -0,0 +1,15 @@
+def add_two_numbers(num1,num2):
+    """
+    returns the sum of two numbers
+
+    Input:
+        num1 : numeric
+        num2 : numeric
+
+    Output:
+        sum of num1 and num2 : numeric
+    """
+    return num1 + num2
+
+def test_add_two_numbers():
+    assert add_two_numbers(3,4) == 7
